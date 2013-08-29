bc15a5a24b60d9cdeeb0d2a083e6d9aa013831f6
Hugo Boyer <hugo@osrfoundation.org>
2013-08-29 10:47:43 -0700

    removed assets

---
+++ gz3d/src/gzscene.js
@@ -0,0 +1,197 @@
+GZ3D.Scene = function()
+{
+  var that = this;
+  this.init();
+};
+
+GZ3D.Scene.prototype.init = function()
+{
+  this.scene = new THREE.Scene();
+  // scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+/*  var geometry = new THREE.CylinderGeometry( 0, 1, 3, 4, 1 );
+  var material =  new THREE.MeshLambertMaterial(
+      { color:0xffffff, shading: THREE.FlatShading } );
+  var mesh = new THREE.Mesh( geometry, material );
+  mesh.position.x = 0;
+  mesh.position.y = 0;
+  mesh.position.z = 0;
+  mesh.updateMatrix();
+  mesh.matrixAutoUpdate = false;
+  this.scene.add( mesh );*/
+
+  this.renderer = new THREE.WebGLRenderer({antialias: false });
+  this.renderer.setClearColor(0xcccccc, 1);
+  this.renderer.setSize( window.innerWidth, window.innerHeight);
+
+  // lights
+  var light = new THREE.AmbientLight( 0x222222 );
+  this.scene.add(light);
+
+  this.camera = new THREE.PerspectiveCamera(
+      60, window.innerWidth / window.innerHeight, 1, 1000 );
+  this.camera.position.x = 0;
+  this.camera.position.y = -5;
+  this.camera.position.z = 5;
+  this.camera.up = new THREE.Vector3(0, 0, 1);
+  this.camera.lookAt(0, 0, 0);
+
+
+  this.controls = new THREE.TrackballControls(this.camera);
+  this.controls.rotateSpeed = 1.0;
+  this.controls.zoomSpeed = 1.2;
+  this.controls.panSpeed = 0.8;
+  this.controls.noZoom = false;
+  this.controls.noPan = false;
+  this.controls.staticMoving = true;
+  this.controls.dynamicDampingFactor = 0.3;
+  this.controls.keys = [ 65, 83, 68 ];
+
+  var that = this;
+  this.controls.addEventListener('change', function() {that.render();});
+
+  this.iface = new GZ3D.GZIface(this);
+
+};
+
+GZ3D.Scene.prototype.getDomElement = function()
+{
+  return this.renderer.domElement;
+};
+
+
+GZ3D.Scene.prototype.render = function()
+{
+  this.renderer.render(this.scene, this.camera);
+  this.controls.update();
+};
+
+GZ3D.Scene.prototype.setWindowSize = function(width, height)
+{
+  this.camera.aspect = width / height;
+  this.camera.updateProjectionMatrix();
+
+  this.renderer.setSize( width, height);
+  this.controls.handleResize();
+  this.render();
+};
+
+GZ3D.Scene.prototype.add = function(model)
+{
+  this.scene.add(model);
+};
+
+GZ3D.Scene.prototype.remove = function(model)
+{
+  this.scene.remove(model);
+};
+
+GZ3D.Scene.prototype.getByName = function(name)
+{
+  return this.scene.getObjectByName(name, true);
+};
+
+GZ3D.Scene.prototype.createGrid = function()
+{
+  var grid = new THREE.GridHelper(10, 1);
+  grid.rotation.x = Math.PI * 0.5;
+  this.scene.add(grid);
+};
+
+GZ3D.Scene.prototype.createSphere = function(radius)
+{
+  var geometry = new THREE.SphereGeometry(radius, 32, 32);
+  var material =  new THREE.MeshPhongMaterial(
+      {color:0xffffff, shading: THREE.SmoothShading} );
+  var mesh = new THREE.Mesh(geometry, material);
+  return mesh;
+};
+
+
+GZ3D.Scene.prototype.createCylinder = function(radius, length)
+{
+  var geometry = new THREE.CylinderGeometry(radius, radius, length, 32, 1,
+      false);
+  var material =  new THREE.MeshPhongMaterial(
+      {color:0xffffff, shading: THREE.SmoothShading} );
+  var mesh = new THREE.Mesh(geometry, material);
+  mesh.rotation.x = Math.PI * 0.5;
+  return mesh;
+};
+
+GZ3D.Scene.prototype.createBox = function(width, height, depth)
+{
+  var geometry = new THREE.CubeGeometry(width, height, depth, 1, 1, 1);
+  var material =  new THREE.MeshPhongMaterial(
+      {color:0xffffff, shading: THREE.SmoothShading} );
+  var mesh = new THREE.Mesh(geometry, material);
+  return mesh;
+};
+
+GZ3D.Scene.prototype.loadURI = function(uri, parent)
+{
+  var uriPath = uri.substring(0, uri.lastIndexOf('/'));
+  var uriFile = uri.substring(uri.lastIndexOf('/') + 1);
+
+  // load urdf model
+  if (uriFile.substr(-4).toLowerCase() === '.dae')
+  {
+    return this.loadCollada(uri, parent);
+  }
+  else if (uriFile.substr(-5).toLowerCase() === '.urdf')
+  {
+    var urdfModel = new ROSLIB.UrdfModel({
+      string : uri
+    });
+
+    // adapted from ros3djs
+    var links = urdfModel.links;
+    for ( var l in links) {
+      var link = links[l];
+      if (link.visual && link.visual.geometry) {
+        if (link.visual.geometry.type === ROSLIB.URDF_MESH) {
+          var frameID = '/' + link.name;
+          var filename = link.visual.geometry.filename;
+          var meshType = filename.substr(-4).toLowerCase();
+          var mesh = filename.substring(filename.indexOf('://') + 3);
+          // ignore mesh files which are not in Collada format
+          if (meshType === '.dae')
+          {
+            var dae = this.loadCollada(uriPath + '/' + mesh, parent);
+            // check for a scale
+            if(link.visual.geometry.scale)
+            {
+              dae.scale = new THREE.Vector3(
+                  link.visual.geometry.scale.x,
+                  link.visual.geometry.scale.y,
+                  link.visual.geometry.scale.z
+              );
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+GZ3D.Scene.prototype.loadCollada = function(uri, parent)
+{
+  var dae;
+  var loader = new THREE.ColladaLoader();
+//  var loader = new ColladaLoader2();
+//  loader.options.convertUpAxis = true;
+  loader.load(uri, function(collada)
+  {
+    // check for a scale factor
+    /*if(collada.dae.asset.unit)
+    {
+      var scale = collada.dae.asset.unit;
+      collada.scene.scale = new THREE.Vector3(scale, scale, scale);
+    }*/
+    dae = collada.scene;
+    dae.updateMatrix();
+    parent.add(dae);
+    //init();
+    //animate();
+  } );
+//  return dae;
+};
