c61343794e69759d4917cfae227efdd50413db37
chapulina <burajiru.no.chapulina@gmail.com>
2014-05-04 12:40:19 -0300

    snap-to-grid option on menu

---
+++ gz3d/src/gzspawnmodel.js
@@ -0,0 +1,229 @@
+/**
+ * Spawn a model into the scene
+ * @constructor
+ */
+GZ3D.SpawnModel = function(scene, domElement)
+{
+  this.scene = scene;
+  this.domElement = ( domElement !== undefined ) ? domElement : document;
+  this.init();
+  this.obj = undefined;
+  this.callback = undefined;
+};
+
+/**
+ * Initialize SpawnModel
+ */
+GZ3D.SpawnModel.prototype.init = function()
+{
+  this.plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
+  this.projector = new THREE.Projector();
+  this.ray = new THREE.Ray();
+  this.obj = null;
+  this.active = false;
+};
+
+/**
+ * Start spawning an entity. Only simple shapes supported so far.
+ * Adds a temp object to the scene which is not registered on the server.
+ * @param {string} entity
+ * @param {function} callback
+ */
+GZ3D.SpawnModel.prototype.start = function(entity, callback)
+{
+  if (this.active)
+  {
+    this.finish();
+  }
+
+  // Kill camera controls
+  this.scene.killCameraControl  = true;
+
+  this.callback = callback;
+
+  this.obj = new THREE.Object3D();
+  var mesh;
+  if (entity === 'box')
+  {
+    mesh = this.scene.createBox(1, 1, 1);
+    this.obj.name = 'unit_box_' +  (new Date()).getTime();
+  }
+  else if (entity === 'sphere')
+  {
+    mesh = this.scene.createSphere(0.5);
+    this.obj.name = 'unit_sphere_' + (new Date()).getTime();
+  }
+  else if (entity === 'cylinder')
+  {
+    mesh = this.scene.createCylinder(0.5, 1.0);
+    this.obj.name = 'unit_cylinder_' + (new Date()).getTime();
+  }
+
+  // temp model appears within current view
+  var pos = new THREE.Vector2(window.window.innerWidth/2, window.innerHeight/2);
+  var intersect = new THREE.Vector3();
+  this.scene.getRayCastModel(pos, intersect);
+
+  this.obj.add(mesh);
+  this.obj.position.x = intersect.x;
+  this.obj.position.y = intersect.y;
+  this.obj.position.z += 0.5;
+  this.scene.add(this.obj);
+
+  var that = this;
+
+  this.mouseDown = function(event) {that.onMouseDown(event);};
+  this.mouseUp = function(event) {that.onMouseUp(event);};
+  this.mouseMove = function(event) {that.onMouseMove(event);};
+  this.keyDown = function(event) {that.onKeyDown(event);};
+  this.touchMove = function(event) {that.onTouchMove(event,true);};
+  this.touchEnd = function(event) {that.onTouchEnd(event);};
+
+  this.domElement.addEventListener('mousedown', that.mouseDown, false);
+  this.domElement.addEventListener( 'mouseup', that.mouseUp, false);
+  this.domElement.addEventListener( 'mousemove', that.mouseMove, false);
+  document.addEventListener( 'keydown', that.keyDown, false);
+
+  this.domElement.addEventListener( 'touchmove', that.touchMove, false);
+  this.domElement.addEventListener( 'touchend', that.touchEnd, false);
+
+  this.active = true;
+
+};
+
+/**
+ * Finish spawning an entity: re-enable camera controls,
+ * remove listeners, remove temp object
+ */
+GZ3D.SpawnModel.prototype.finish = function()
+{
+  // Re-enable camera controls
+  this.scene.killCameraControl = false;
+  var that = this;
+
+  this.domElement.removeEventListener( 'mousedown', that.mouseDown, false);
+  this.domElement.removeEventListener( 'mouseup', that.mouseUp, false);
+  this.domElement.removeEventListener( 'mousemove', that.mouseMove, false);
+  document.removeEventListener( 'keydown', that.keyDown, false);
+
+  this.scene.remove(this.obj);
+  this.obj = undefined;
+  this.active = false;
+};
+
+/**
+ * Window event callback
+ * @param {} event - not yet
+ */
+GZ3D.SpawnModel.prototype.onMouseDown = function(event)
+{
+  // Does this ever get called?
+  // Change like this:
+  // https://bitbucket.org/osrf/gzweb/pull-request/14/switch-to-arrow-mode-when-spawning-models/diff
+  event.preventDefault();
+  event.stopImmediatePropagation();
+};
+
+/**
+ * Window event callback
+ * @param {} event - mousemove events
+ */
+GZ3D.SpawnModel.prototype.onMouseMove = function(event)
+{
+  if (!this.active)
+  {
+    return;
+  }
+
+  event.preventDefault();
+
+  this.moveSpawnedModel(event.clientX,event.clientY);
+};
+
+/**
+ * Window event callback
+ * @param {} event - touchmove events
+ */
+GZ3D.SpawnModel.prototype.onTouchMove = function(event,originalEvent)
+{
+  if (!this.active)
+  {
+    return;
+  }
+
+  var e;
+
+  if (originalEvent)
+  {
+    e = event;
+  }
+  else
+  {
+    e = event.originalEvent;
+  }
+  e.preventDefault();
+
+  if (e.touches.length === 1)
+  {
+    this.moveSpawnedModel(e.touches[ 0 ].pageX,e.touches[ 0 ].pageY);
+  }
+};
+
+/**
+ * Window event callback
+ * @param {} event - touchend events
+ */
+GZ3D.SpawnModel.prototype.onTouchEnd = function()
+{
+  if (!this.active)
+  {
+    return;
+  }
+
+  this.callback(this.obj);
+  this.finish();
+};
+
+/**
+ * Window event callback
+ * @param {} event - mousedown events
+ */
+GZ3D.SpawnModel.prototype.onMouseUp = function(event)
+{
+  if (!this.active)
+  {
+    return;
+  }
+
+  this.callback(this.obj);
+  this.finish();
+};
+
+/**
+ * Window event callback
+ * @param {} event - keydown events
+ */
+GZ3D.SpawnModel.prototype.onKeyDown = function(event)
+{
+  if ( event.keyCode === 27 ) // Esc
+  {
+    this.finish();
+  }
+};
+
+/**
+ * Move temp spawned model
+ * @param {integer} positionX - Horizontal position on the canvas
+ * @param {integer} positionY - Vertical position on the canvas
+ */
+GZ3D.SpawnModel.prototype.moveSpawnedModel = function(positionX,positionY)
+{
+  var vector = new THREE.Vector3( (positionX / window.containerWidth) * 2 - 1,
+        -(positionY / window.containerHeight) * 2 + 1, 0.5);
+  this.projector.unprojectVector(vector, this.scene.camera);
+  this.ray.set(this.scene.camera.position,
+      vector.sub(this.scene.camera.position).normalize());
+  var point = this.ray.intersectPlane(this.plane);
+  point.z = this.obj.position.z;
+  this.scene.setPose(this.obj, point, new THREE.Quaternion());
+};
